e6946aa61995a0886c0207377c39cb2e4cc527a7
Neohammer <m.marceau@gmail.com>
2023-09-16 15:33:10 +0200

    Add message to action

--- src/Response/Action.php
+++ src/Response/Action.php
@@ -4,6 +4,8 @@ namespace Architekt\Response;
 
 class Action extends BaseResponse
 {
+    private ?string $message = null;
+
     public function __construct()
     {
         parent::init([]);
@@ -13,5 +15,31 @@ class Action extends BaseResponse
     {
         echo json_encode($this->buildRoute());
         exit();
+    }
+
+    public function message(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    protected function buildRoute(): array
+    {
+        return array_merge(
+            parent::buildRoute(),
+            $this->buildMessage()
+        );
+    }
+
+    private function buildMessage(): array
+    {
+        if (null !== $this->message) {
+            return [
+                'message' => $this->message,
+            ];
+        }
+
+        return [];
     }
 }
