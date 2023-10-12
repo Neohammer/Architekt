9dfc83be720000b104409dd79f42870341df97e8
Neohammer <m.marceau@gmail.com>
2023-10-12 09:36:22 +0200

    Clean library/file

--- src/Library/File.php
+++ src/Library/File.php
@@ -13,96 +13,27 @@ use Gmao\PreventivePlanRangeOperation;
 use Gmao\Subset;
 use Gmao\WorkOrder;
 
+if (!defined('ARCHITEKT_DATATABLE_PREFIX')) {
+    define('ARCHITEKT_DATATABLE_PREFIX', 'at_');
+}
+
 class File extends DBEntity
 {
     use DBEntityCache;
 
     private const DIRECTORY_DEPTH = 2;
     private const DIRECTORY_LETTERS = 1;
-    public const ACCESS_ADMIN = 0;
-    public const ACCESS_CHIEF = 10;
-    public const ACCESS_PUBLIC = 20;
-    public const TYPE_OPERATION = 'operation';
-    public const TYPE_EQUIPMENT = 'equipment';
-    public const TYPE_WORK_ORDER = 'workOrder';
-
-    static public array $allowedTypes = [
-        self::TYPE_OPERATION,
-        self::TYPE_EQUIPMENT,
-        self::TYPE_WORK_ORDER
-    ];
-
-    static public array $allowedAccesses = [
-        self::ACCESS_ADMIN,
-        self::ACCESS_CHIEF,
-        self::ACCESS_PUBLIC
-    ];
-
+    
     private static bool $transactionStarted = false;
     private static array $transactionFiles;
+
+    protected static ?string $_table_prefix = ARCHITEKT_DATATABLE_PREFIX;
     protected static ?string $_table = 'file';
-
-    public static function preventivePlanRangeList(PreventivePlanRange $preventivePlanRange): array
-    {
-        $that = new self;
-        $operation = new Operation();
-
-        $that->_search()
-            ->join($operation, $operation->_primaryKey(), $that, 'external_id')
-            ->filterOn('external_type', self::TYPE_OPERATION)
-            ->join(new PreventivePlanRangeOperation(), $operation->_strangerKey(), $operation);
-
-        return $that->_results();
-    }
-
-    public static function workOrderList(WorkOrder $workOrder): array
-    {
-        $that = new self;
-        $that->_search()->filter([
-            'external_type' => File::TYPE_WORK_ORDER,
-            'external_id' => $workOrder
-        ]);
-
-        return $that->_results();
-    }
-
-    public static function operationList(Operation $operation)
-    {
-        $that = new self;
-        $that->_search()->filter([
-            'external_type' => self::TYPE_OPERATION,
-            'external_id' => $operation->_primary()
-        ]);
-
-        return $that->_results();
-    }
-
-    /**
-     * @return static[]
-     */
-    public static function equipmentList(Equipment $equipment): array
-    {
-        $that = new self;
-        $that->_search()->filter($equipment);
-
-        return $that->_results();
-    }
-
-    /**
-     * @return static[]
-     */
-    public static function subsetList(Subset $subset): array
-    {
-        $that = new self;
-        $that->_search()->filter($subset);
-
-        return $that->_results();
-    }
 
     public static function createFromContent(string $content, string $extension, int $sourceId): ?self
     {
         $hash = md5($content);
-        $path = File . phpself::getBasePath();
+        $path = self::getBasePath();
         $directory = self::getRelativePath($hash);
         $filename = $path . '/' . $hash;
 
@@ -129,10 +60,10 @@ class File extends DBEntity
     public static function upload(UploadFile $uploadFile, ?File $file = null): ?self
     {
         $fileHash = md5_file($uploadFile->temporaryName());
-        $fileUniq = md5(File . phptime());
+        $fileUniq = md5(uniqid().time());
 
         $fileRelativePath = static::getRelativePath($fileUniq);
-        $filename = File . phpself::getBasePath() . $fileRelativePath . DIRECTORY_SEPARATOR . $fileUniq;
+        $filename = self::getBasePath() . $fileRelativePath . DIRECTORY_SEPARATOR . $fileUniq;
         $move = @move_uploaded_file(
             $uploadFile->temporaryName(),
             $filename
@@ -164,10 +95,10 @@ class File extends DBEntity
     public static function import(string $filePath, ?File $file = null, $deleteFile = false): ?self
     {
         $fileHash = md5_file($filePath);
-        $fileUniq = md5(File . phptime());
+        $fileUniq = md5(uniqid().time());
 
         $fileRelativePath = static::getRelativePath($fileUniq);
-        $filename = File . phpself::getBasePath() . $fileRelativePath . DIRECTORY_SEPARATOR . $fileUniq;
+        $filename = self::getBasePath() . $fileRelativePath . DIRECTORY_SEPARATOR . $fileUniq;
 
         $move = copy($filePath,$filename);
         if (false === $move) {
@@ -238,38 +169,6 @@ class File extends DBEntity
         }
     }
 
-    public static function accessLabels(): array
-    {
-        return [
-            self::ACCESS_PUBLIC => 'Public',
-            self::ACCESS_CHIEF => 'Chef d\'équipe',
-            self::ACCESS_ADMIN => 'Administrateur',
-        ];
-    }
-
-    public static function defaultAccess(): string
-    {
-        return self::ACCESS_ADMIN;
-    }
-
-    public function userHasAccess(User $user): bool
-    {
-        if ($user->isGmaoAdmin()) {
-            return true;
-        }
-
-        if ($user->isTeamLeader() && $this->_get('access') >= self::ACCESS_CHIEF) {
-            return true;
-        }
-
-        return $this->_get('access') >= self::ACCESS_PUBLIC;
-    }
-
-    public function accessLabel(): string
-    {
-        return self::accessLabels()[$this->_get('access')];
-    }
-
     public function label(): string
     {
         return $this->_get('title');
@@ -278,21 +177,6 @@ class File extends DBEntity
     public function author(): User
     {
         return User::fromCache($this->_get('author_id'));
-    }
-
-    public function category(): FileCategory
-    {
-        return FileCategory::fromCache($this->_get('file_category_id'));
-    }
-
-    public function equipment(): Equipment
-    {
-        return Equipment::fromCache($this->_get('equipment_id'));
-    }
-
-    public function subset(): Subset
-    {
-        return Subset::fromCache($this->_get('subset_id'));
     }
 
     public function external(): ?Entity
@@ -341,7 +225,7 @@ class File extends DBEntity
     public function read(): string
     {
         return readfile(
-            File . phpself::getBasePath()
+            self::getBasePath()
             . DIRECTORY_SEPARATOR
             . self::_get('uniqid'));
     }
@@ -353,7 +237,7 @@ class File extends DBEntity
 
     private function unlink(): bool
     {
-        $file = File . phpself::getBasePath()
+        $file = self::getBasePath()
             . self::getRelativePath($this->_get('hash'))
             . $this->_get('hash');
 
@@ -372,8 +256,8 @@ class File extends DBEntity
                     (self::DIRECTORY_LETTERS * $i),
                     self::DIRECTORY_LETTERS + (self::DIRECTORY_LETTERS * $i)
                 );
-            if (!is_dir(File . phpself::getBasePath())) {
-                mkdir(File . phpself::getBasePath(), 07777);
+            if (!is_dir(self::getBasePath())) {
+                mkdir(self::getBasePath(), 07777);
             }
         }
         return $path;
