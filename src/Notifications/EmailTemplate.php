b84027633f4eaa3f02006158fb1c446baf4b8b45
Neohammer <m.marceau@gmail.com>
2023-12-22 09:29:28 +0100

    Path template compile path

--- src/Notifications/EmailTemplate.php
+++ src/Notifications/EmailTemplate.php
@@ -10,7 +10,7 @@ class EmailTemplate extends Smarty
     public function init(): self
     {
         $this->setTemplateDir(Application::$configurator->get('path') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'Notifications' . DIRECTORY_SEPARATOR . 'Emails')
-            ->setCompileDir(PATH_FILER . 'Smarty/compile/');
+            ->setCompileDir(PATH_FILER . '/Smarty/compile/');
 
         return $this;
     }
