e02be133b0c46b4394832591bdbd4f063be84d43
Neohammer <m.marceau@gmail.com>
2023-12-30 12:13:55 +0100

    Add formatter to email template

--- src/Notifications/EmailTemplate.php
+++ src/Notifications/EmailTemplate.php
@@ -3,6 +3,7 @@
 namespace Architekt\Notifications;
 
 use Architekt\Application;
+use Architekt\View\Formatter;
 use Smarty;
 
 class EmailTemplate extends Smarty
@@ -10,7 +11,8 @@ class EmailTemplate extends Smarty
     public function init(): self
     {
         $this->setTemplateDir(Application::$configurator->get('path') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'Notifications' . DIRECTORY_SEPARATOR . 'Emails')
-            ->setCompileDir(PATH_FILER . '/Smarty/compile/');
+            ->setCompileDir(PATH_FILER . '/Smarty/compile/')
+            ->registerObject('Formatter', new Formatter());
 
         return $this;
     }
